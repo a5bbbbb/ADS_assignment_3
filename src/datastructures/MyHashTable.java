84176f8c798e2f2747970c1e6457334863bb3676
a5bbbbb <suprunoviktor@gmail.com>
2024-05-14 22:07:07 +0500

    Commit 2: Hash Table with commented code.

---
+++ src/datastructures/MyHashTable.java
@@ -0,0 +1,314 @@
+package datastructures;
+
+import java.util.ArrayList;
+import java.util.NoSuchElementException;
+
+import static java.lang.Math.abs;
+
+public class MyHashTable <K, V>{
+
+    private class HashNode<K, V>{
+        private K key;
+        private V value;
+        private HashNode<K, V> next;
+
+        public HashNode(K key, V value){
+            this.key = key;
+            this.value = value;
+        }
+
+        /*
+         * Finds the number of nodes until the end of the bucket.
+         *
+         * Time complexity: O(N), where N is the number of nodes in the bucket.
+         *
+         * @return the number of nodes until the end of the bucket.
+         */
+        private int chainLength(){
+            int length = 0;
+            HashNode<K, V> node = this;
+            while(node != null){
+                length++;
+                node = node.next;
+            }
+            return length;
+        }
+
+        @Override
+        public String toString(){
+            return "{" + key + " " + value + "}";
+        }
+    }
+    private Object[] chainArray;
+    private int M = 11;
+    private int size = 0;
+
+    private void initializeChainArray(){
+        chainArray = new Object[M];
+    }
+
+    /*
+     * Constructor with default number of buckets.
+     */
+    public MyHashTable(){
+        initializeChainArray();
+    }
+
+    /*
+     * Constructor with custom number of buckets.
+     */
+    public MyHashTable(int M){
+        this.M = M;
+        initializeChainArray();
+    }
+
+    /*
+    * Checks if the object passed is null.
+    *
+    * Time complexity: O(1).
+    *
+    * @param o the object to be checked.
+    * @throws NullPointerException if the object passed is null.
+     */
+    private void checkNull(Object o){
+        if(o == null)
+            throw new NullPointerException();
+    }
+
+    /*
+    * Returns index of the bucket by hash of the key.
+    * The bucket is determined by key.hashCode() % M.
+    * Where M is number of buckets.
+    *
+    * Time complexity is that of the key's hashCode method.
+    *
+    * @param key the key to an object in HashTable.
+    * @return index of the bucket of the key.
+     */
+    private int hash(K key){
+        checkNull(key);
+        return (key.hashCode()%M + M)%M;
+    }
+
+    /*
+    * Doubles number of buckets when the load of the table exceeds the load factor.
+    *
+    * Time complexity: O(N), where N is the number of pairs inserted.
+     */
+    private void resize(){
+        M *= 2;
+        Object[] oldChainArray = chainArray;
+        initializeChainArray();
+        for(var t : oldChainArray){
+            if(t == null)continue;
+            HashNode<K, V> node = (HashNode<K, V>)t;
+            while(node != null){
+                // Put every key-value pair from old chainArray to new one.
+                put(node.key, node.value);
+                // The size increased using put method, but actual size stays the same.
+                size--;
+                node = node.next;
+            }
+        }
+    }
+
+    /*
+    * Puts the key, value pair in the HashTable.
+    * Finds the bucket of the key through hash() method.
+    * If some node already has identical key,
+    * then changes value of the node to value argument.
+    * Otherwise, puts node with the pair in the front of the chain of the bucket.
+    * Resizes the table if the load exceeds the load factor.
+    *
+    * Time complexity: O(N), where N is the number of pairs inserted; Θ(1).
+    *
+    * @param key the key to the value.
+    * @param value the value for the key.
+     */
+    public void put(K key, V value){
+        checkNull(key);
+        HashNode<K, V> existingNode;
+        try{
+            // If node with key=key already exists, then change value.
+            existingNode = getNodeByKey(key);
+            existingNode.value = value;
+        }catch (NoSuchElementException e){
+            // If node with key=key does not exist, then put new node to the head of the chain.
+            int index = hash(key);
+            HashNode<K, V> newNode = new HashNode<>(key, value);
+            newNode.next = (HashNode<K, V>) chainArray[index];
+            chainArray[index] = newNode;
+            size++;
+            // Resize the table if the load exceeds the load factor.
+            if((double)size/M > 0.7)
+                resize();
+        }
+    }
+
+    /*
+     * Helper method. Finds the node by the key.
+     * Finds the bucket of the key through hash() method.
+     * If the bucket is empty or cannot find node with key=key, then return null.
+     * Otherwise, return node with key=key.
+     *
+     * Time complexity: O(N), where N is the number of pairs inserted; Θ(1);
+     *
+     * @param key the key of the node.
+     * @return the node with key=key;
+     * @throws NoSuchElementException if no node with key=key was found.
+     */
+    private HashNode<K, V> getNodeByKey(K key){
+        checkNull(key);
+        HashNode<K, V> node = (HashNode<K, V>) chainArray[hash(key)];
+        while(node != null && !node.key.equals(key))
+            node = node.next;
+        if(node == null) {
+            throw new NoSuchElementException("No value with key = " + key + " was found.");
+        }
+        return node;
+    }
+
+    /*
+     * Finds the value by the key.
+     * Finds the bucket of the key through hash() method.
+     * If the bucket is empty or cannot find node with key=key, then throw NoSuchElementException.
+     * Otherwise, return the node with key=key.
+     *
+     * Time complexity: O(N), where N is the number of pairs inserted; Θ(1);
+     *
+     * @param key the key of the value.
+     * @return the node with key=key;
+     * @throws NoSuchElementException if value with key=key was found.
+     */
+    public V get(K key){
+        checkNull(key);
+        HashNode<K, V> node = getNodeByKey(key);
+        return node.value;
+    }
+
+    /*
+     * Helper method. Finds the previous node of the node with key=key.
+     * Finds the bucket of the key through hash() method.
+     * If the bucket is empty or cannot find node with key=key, then throw NoSuchElementException.
+     * Otherwise, return the previous node of the node with key=key.
+     *
+     * Time complexity: O(N), where N is the number of pairs inserted; Θ(1);
+     *
+     * @param key the key of the node.
+     * @return the previous node of the node with key=key;
+     * @throws NoSuchElementException if no node with key=key was found.
+     */
+    private HashNode<K, V> getPrevNodeByKey(K key){
+        checkNull(key);
+        HashNode<K, V> node = (HashNode<K, V>) chainArray[hash(key)], prev = null;
+        while(node != null && !node.key.equals(key)) {
+            prev = node;
+            node = node.next;
+        }
+        if(node == null)
+            throw new NoSuchElementException("No value with key = " + key + " was found.");
+        return prev;
+    }
+
+    /*
+     * Removes the key-value pair from the table.
+     * Finds the bucket of the key through hash() method.
+     * If the bucket is empty or cannot find node with key=key, then throw NoSuchElementException.
+     * Otherwise, delete the node with key=key, and return the value of the node.
+     *
+     * Time complexity: O(N), where N is the number of pairs inserted; Θ(1);
+     *
+     * @param key the key of the value.
+     * @return the value of the removed node.
+     * @throws NoSuchElementException if value with key=key was found.
+     */
+    public V remove(K key) {
+        checkNull(key);
+        int index = hash(key);
+        HashNode<K, V> prevNode = getPrevNodeByKey(key);
+        V value;
+        // If the previous node is null, then the node to be removed is the head of the bucket.
+        if (prevNode == null) {
+            value = ((HashNode<K, V>) chainArray[index]).value;
+            chainArray[index] = null;
+        }
+        // Otherwise, the next node of the previous node is the next of the node to be removed.
+        else {
+            value = prevNode.next.value;
+            prevNode.next = prevNode.next.next;
+        }
+        size--;
+        return value;
+    }
+
+    /*
+     * Checks if the value with key=key is in the table.
+     * Finds the bucket of the key through hash() method.
+     * If the bucket is empty or cannot find node with key=key, then throw NoSuchElementException.
+     * Otherwise, delete the node with key=key, and return the value of the node.
+     *
+     * Time complexity: O(N), where N is the number of pairs inserted; Θ(1);
+     *
+     * @param key the key of the value.
+     * @return true if the value with key=key is in the table, false otherwise.
+     * @throws NoSuchElementException if value with key=key was found.
+     */
+    public boolean contains(K key){
+        try {
+            getNodeByKey(key);
+            return true;
+        }catch(Exception e) {
+            return false;
+        }
+    }
+    /*
+     * Finds the key with value=value.
+     * Brute force through the table.
+     *
+     * Time complexity: O(N+M), where N is the number of pairs inserted,
+     * and M is the number of buckets.
+     *
+     * @param value the value of the node.
+     * @return the key of the value;
+     * @throws NoSuchElementException if no key with value=value was found.
+     */
+    public K getKey(V value){
+        for(var t : chainArray){
+            HashNode<K, V> node = (HashNode<K, V>)t;
+            while(node != null){
+                if(node.value == value)return node.key;
+                node = node.next;
+            }
+        }
+        throw new NoSuchElementException("No key with value = " + value + " was found.");
+    }
+
+    /*
+     * Returns sizes of buckets of the table.
+     *
+     * Time complexity: O(N+M), where N is the number of pairs inserted,
+     * and M is the number of buckets.
+     *
+     * @return sizes of buckets.
+     */
+    public ArrayList<Integer> getBucketSizes(){
+        ArrayList<Integer> bucketSizes = new ArrayList<>();
+        for(var t : chainArray) {
+            HashNode<K, V> node = (HashNode<K, V>)t;
+            if (node == null) {
+                bucketSizes.add(0);
+            } else {
+                bucketSizes.add(node.chainLength());
+            }
+        }
+        return bucketSizes;
+    }
+
+    public int size(){
+        return size;
+    }
+
+    public int getM() {
+        return M;
+    }
+}
