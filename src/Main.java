532e3fe41f9ee1c955c9c7c2042aac2aff741563
a5bbbbb <suprunoviktor@gmail.com>
2024-05-15 21:41:46 +0500

    Commit 11: Subdivided code into more methods. Minor changes.

--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,10 @@ public class Main {
     private static final Random rnd = new Random();
     private static final int sampleSize = 10000;
 
-    public static String getRandomString(int length) {
+    private static final MyHashTable<MyTestingClass, Integer> table = new MyHashTable<>();
+    private static final BST<Integer, Integer> tree = new BST<>();
+
+    private static String getRandomString(int length) {
         String dict = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
         StringBuilder builder = new StringBuilder();
         while (builder.length() < length) { // length of the random string.
@@ -22,19 +25,13 @@ public class Main {
         return builder.toString();
     }
 
-    public static void testMyHashTable(){
-        MyHashTable<MyTestingClass, Integer> table = new MyHashTable<>();
-
-        long startTime = System.currentTimeMillis();
-
+    private static void insertHashTable(){
         for (int i = 0; i < sampleSize; i++){
             table.put(new MyTestingClass(getRandomString(20)), i);
         }
+    }
 
-        long stopTime = System.currentTimeMillis();
-        long elapsedTime = stopTime - startTime;
-        System.out.println("Elapsed time put " + sampleSize + " : " + elapsedTime + " mils");
-
+    private static void printBucketsAndStatistics(){
         List<Integer> bucketSizes = table.getBucketSizes();
         double usedBuckets = 0, sum = 0, max = 0, aboveAvg = 0;
         for(int i = 0; i < bucketSizes.size(); i++){
@@ -55,30 +52,29 @@ public class Main {
         System.out.println("Unique keys: " + table.size());
     }
 
-    public static void testBST(){
-        BST<Integer, Integer> tree = new BST<>();
-
-        ArrayList<Integer> arr = new ArrayList<>();
-
+    private static void testMyHashTable(){
         long startTime = System.currentTimeMillis();
+        insertHashTable();
+        long stopTime = System.currentTimeMillis();
+        long elapsedTime = stopTime - startTime;
+        printBucketsAndStatistics();
+        System.out.println("Elapsed time put " + sampleSize + " : " + elapsedTime + " mils");
+    }
 
+    private static void testBST(){
+        ArrayList<Integer> arr = new ArrayList<>();
+        long startTime = System.currentTimeMillis();
         for (int i = 0; i < 10; i++){
             arr.add(rnd.nextInt());
             tree.put(arr.getLast(), i);
         }
-
         long stopTime = System.currentTimeMillis();
         long elapsedTime = stopTime - startTime;
-
         for (var elem : tree) {
-
             System.out.println("key is " + elem.getKey() + " and value is " + elem.getValue());
-
         }
-
         System.out.println("Tree size: " + tree.size());
-
-        System.out.println("Elapsed time put " + sampleSize + " : " + elapsedTime + " mils");
+        System.out.println("Elapsed time put 10 : " + elapsedTime + " mils");
     }
 
     public static void main(String[] args){
